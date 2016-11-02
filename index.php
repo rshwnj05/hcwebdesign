4f56d422bd0fcedcd051f390bfe709c6e51dc93b
rshwnj05 <rshwnj05@yahoo.com>
2016-11-02 10:52:02 -0600

    added php,css,js files

---
+++ index.php
@@ -0,0 +1,174 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>HC WebDesign</title>
+    <?php require_once("lib/headUtils.php");?>
+  </head>
+  <body>
+
+    <?php require_once("lib/partials/navbar.php");?>
+    <?php require_once("lib/partials/modal.php");?>
+    <?php require_once("lib/partials/carousel.php");?>
+
+    <section class="marketing">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <i class="fa fa-user fa-5x fa-border"></i>
+            <h2>24/7 Support</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              <a class="btn btn-default" href="#">View Details</a>
+            </p>
+          </div>
+          <div class="col-md-4">
+            <i class="fa fa-wordpress fa-5x fa-border"></i>
+            <h2>One Click Installation</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              <a class="btn btn-default" href="#">View Details</a>
+            </p>
+          </div>
+          <div class="col-md-4">
+            <i class="fa fa-server fa-5x fa-border"></i>
+            <h2>Daily Backups</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              <a class="btn btn-default" href="#">View Details</a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </section>
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-6 col-sm col-md-6 col-md-3 col-lg-3">
+            <div class="panel price panel grey">
+              <div class="panel-heading arrow_box text-center">
+                <h3>Starter Plan</h3>
+              </div>
+              <div class="panel-body text-center">
+                <p class="lead"><strong>$3.99/month</strong>
+                </p>
+              </div>
+              <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item">1 Free Domain</li>
+                <li class="list-group-item">1GB Disc Space</li>
+                <li class="list-group-item">10 GB Bandwidth</li>
+                <li class="list-group-item">5 Email Accounts</li>
+              </ul>
+              <div class="panel-footer">
+                <a class="btn btn-lg btn-block btn-primary" href="#">BUY NOW</a>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-6 col-sm col-md-6 col-md-3 col-lg-3">
+            <div class="panel price panel grey">
+              <div class="panel-heading arrow_box text-center">
+                <h3>Simple Plan</h3>
+              </div>
+              <div class="panel-body text-center">
+                <p class="lead"><strong>$8.99/month</strong>
+                </p>
+              </div>
+              <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item">1 Free Domain</li>
+                <li class="list-group-item">1GB Disc Space</li>
+                <li class="list-group-item">25GB Bandwidth</li>
+                <li class="list-group-item">20 Email Accounts</li>
+              </ul>
+              <div class="panel-footer">
+                <a class="btn btn-lg btn-block btn-primary" href="#">BUY NOW</a>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-6 col-sm col-md-6 col-md-3 col-lg-3">
+            <div class="panel price panel grey">
+              <div class="panel-heading arrow_box text-center">
+                <h3>Standard Plan</h3>
+              </div>
+              <div class="panel-body text-center">
+                <p class="lead"><strong>$12.99/month</strong>
+                </p>
+              </div>
+              <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item">1 Free Domain</li>
+                <li class="list-group-item">35GB Disc Space</li>
+                <li class="list-group-item">50 GB Bandwidth</li>
+                <li class="list-group-item">Unlimited Email Accounts</li>
+              </ul>
+              <div class="panel-footer">
+                <a class="btn btn-lg btn-block btn-primary" href="#">BUY NOW</a>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-6 col-sm col-md-6 col-md-3 col-lg-3">
+            <div class="panel price panel grey">
+              <div class="panel-heading arrow_box text-center">
+                <h3>Super Plan</h3>
+              </div>
+              <div class="panel-body text-center">
+                <p class="lead"><strong>$19.99/month</strong>
+                </p>
+              </div>
+              <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item">2 Free Domains</li>
+                <li class="list-group-item">80GB Disc Space</li>
+                <li class="list-group-item">100 GB Bandwidth</li>
+                <li class="list-group-item">Unlimited Email Accounts</li>
+              </ul>
+              <div class="panel-footer">
+                <a class="btn btn-lg btn-block btn-primary" href="#">BUY NOW</a>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <div class="container">
+        <p class="pull-right">
+          <a href="#">Back to Top</a>
+        </p>
+        <p>
+          EDX Hosting &copy; 2016. &middot;<a href="#">privacy</a>&middot;<a href="#">terms</a>
+        </p>
+      </div>
+    </footer>
+
+  </body>
+</html>
---
+++ lib/partials/carousel.php
@@ -0,0 +1,100 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="lib/images/bg1.jpg">
+      <div class="container">
+        <div class="carousel-caption">
+          <div class="row">
+            <div class="col-md-7">
+              <h1>Affordable Web Hosting</h1>
+              <ul class="feature list">
+                <li><i class="fa fa-check"></i>Unlimited Disk Space</li>
+                <li><i class="fa fa-check"></i>FREE Domain Registry</li>
+                <li><i class="fa fa-check"></i>FREE Suite Security</li>
+                <li><i class="fa fa-check"></i>FREE Site-Building Tools</li>
+                <li><i class="fa fa-check"></i>FREE Online Store</li>
+                <li><i class="fa fa-check"></i>FREE Search Engine & Marketing Credits</li>
+                <li><i class="fa fa-check"></i>GREEN! Powered by 100% Wing Energy</li>
+              </ul>
+              <p>
+                <a class="btn btn-primary" href="#">Sign Up Today</a>
+              </p>
+            </div>
+            <div class="col-md-5">
+              <img src="lib/images/servers1.png"/>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="item">
+      <img src="lib/images/bg2.jpg">
+      <div class="carousel-caption">
+        <div class="row">
+          <div class="col-md-7">
+            <h1>24/7 Support</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              <a class="btn btn-primary" href="#">Sign Up Today</a>
+            </p>
+          </div>
+          <div class="col-md-5">
+            <img src="lib/images/servers2.png"/>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="lib/images/bg3.jpg">
+      <div class="carousel-caption">
+        <div class="row">
+          <div class="col-md-7">
+            <h1>99.9% Uptime</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              <a class="btn btn-primary" href="#">Sign Up Today</a>
+            </p>
+          </div>
+          <div class="col-md-5">
+            <img src="lib/images/servers3.png"/>
+          </div>
+        </div>
+      </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span id="left"class="fa fa-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span id="right" class="fa fa-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
